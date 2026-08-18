6fd6338cb10b9eed0442416937d95a51c000e511
YaswithaThirupati <yaswithathirupati78@gmail.com>
2026-08-19 00:14:35 +0530

    Create day1.py for basic variables and input
    Added a basic Python script for variable demonstration and patient details input.

---
+++ day1.py
@@ -0,0 +1,26 @@
+#day1  of python  learning(basic variables & input)
+"""
+str="yaswitha"
+int=7
+float=10.0
+double=10.00
+print(str)
+print(int)
+print(float)
+print(double)
+
+
+#type of variables
+print(type(str))
+print(type(int))
+print(type(float))
+print(type(double))
+"""
+
+#basic program to print patient details
+name=input("enter patient_name:")
+age=int(input("enter patient_age:"))
+disease=input("enter patient_disease:")
+print("patient_name:",name)
+print("patient_age:",age)
+print("patient_disease:",disease)
